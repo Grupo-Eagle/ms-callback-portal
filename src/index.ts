b45d9658eb04b2cb01920e9cbb8c7a8e39e614a6
RycherdDionizio <123411298+RycherdDionizio@users.noreply.github.com>
2023-08-18 15:05:14 -0300

    salvando

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-console.log("ola mundo", 
+import { PrismaClient } from "@prisma/client";
 
 
+export const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + "?pool_timeout=40&connection_limit=23",
+      },
+    },
+  });
 
-
-
-
-1) ;
+prisma.
